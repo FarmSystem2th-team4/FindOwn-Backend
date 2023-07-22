7053c73841643610e82c1ae8f7784339c7de1198
dlawjddn <jwl5015@naver.com>
2023-07-22 23:55:00 +0900

    Style: 오타수정, 미사용 import 제거

--- src/main/java/Farm/Team4/findOwn/dto/MemberRequestInfo.java
+++ src/main/java/Farm/Team4/findOwn/dto/MemberRequestInfo.java
@@ -6,14 +6,14 @@ import lombok.Getter;
 @Getter
 public class MemberRequestInfo {
     private String id;
-    private String passWord;
+    private String password;
     private String name;
     private String phoneNumber;
     private String email;
     public Member toMember(){
         return new Member(
                 this.id,
-                this.passWord,
+                this.password,
                 this.name,
                 this.phoneNumber,
                 this.email);
