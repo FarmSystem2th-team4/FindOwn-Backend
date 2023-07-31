2cc8bf74c63bb012ced99a106f1ce349042c9d96
dlawjddn <jwl5015@naver.com>
2023-07-31 22:12:58 +0900

    Feat: 상표권 도메인 생성

---
+++ src/main/java/Farm/Team4/findOwn/domain/Trademark.java
@@ -0,0 +1,20 @@
+package Farm.Team4.findOwn.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Trademark {
+    @Id
+    private Long id;
+    private String image;
+    private String applicant;
+    private String markClass;
+    private Long registerNum;
+    private String state;
+    /*
+    private List<String> classification = new ArrayList<>(); // 상표 구분
+
+     */
+
+}
