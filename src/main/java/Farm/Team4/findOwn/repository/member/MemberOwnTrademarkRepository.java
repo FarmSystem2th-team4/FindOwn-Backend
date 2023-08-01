b76458185f6081ecb8b379b8d1f5e705177accb0
dlawjddn <jwl5015@naver.com>
2023-08-02 02:59:08 +0900

    Feat: MemeberOwn~ 레포지토리 생성

---
+++ src/main/java/Farm/Team4/findOwn/repository/member/MemberOwnTrademarkRepository.java
@@ -0,0 +1,7 @@
+package Farm.Team4.findOwn.repository.member;
+
+import Farm.Team4.findOwn.domain.member.MemberOwnTrademark;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberOwnTrademarkRepository extends JpaRepository<MemberOwnTrademark, Long> {
+}
