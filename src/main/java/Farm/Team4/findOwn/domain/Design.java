0d2cef90298d72e584e3c298bcb7141f114c3673
dlawjddn <jwl5015@naver.com>
2023-08-01 10:42:35 +0900

    디자인 구분, 상표 구분 추가

--- src/main/java/Farm/Team4/findOwn/domain/Design.java
+++ src/main/java/Farm/Team4/findOwn/domain/Design.java
@@ -1,9 +1,13 @@
 package Farm.Team4.findOwn.domain;
 
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,12 +20,7 @@ public class Design {
     private String designClass; //  한국분류(디자인 분류에는 국제분류도 있고 한국 분류도 있다), 국제분류를 따르는 경우 한국분류 값이 없는 디자인권들이 존재
     private Long registerNum; // 등록 번호, '-'을 제외한 출원번호 전체
     private String state; // 법적 상태
-
-
-    /*
+    @ElementCollection
     private List<String> classification = new ArrayList<>(); // 디자인 구분(시각 디자인, 편집 디자인 등등), 하나의 디자인은 여러 구분을 가질 수 있음
-
-
-     */
 
 }
