9a96aade3052a871e9f146a74219f84930737cdc
dlawjddn <jwl5015@naver.com>
2023-08-04 18:09:10 +0900

    Fix: 회원 정보 조회 수정

--- src/main/java/Farm/Team4/findOwn/service/member/right/MemberRightService.java
+++ src/main/java/Farm/Team4/findOwn/service/member/right/MemberRightService.java
@@ -32,9 +32,9 @@ public class MemberRightService {
         designService.saveDesign(ownDesign);
         log.info("디자인권 정보 저장 완료");
 
-        Member findMember = memberService.findById(request.getMemberId());
-        if (!memberService.existedMemberByEmail(findMember.getEmail()))
+        if(!memberService.existedMemberById(request.getMemberId()))
             throw new IllegalArgumentException("존재하지 않는 회원입니다.");
+        Member findMember = memberService.findById(request.getMemberId());
         log.info("회원 찾기 완료");
 
         MemberOwnDesign savedInfo = memberOwnDesignRepository.save(new MemberOwnDesign(ownDesign, findMember));
@@ -46,9 +46,9 @@ public class MemberRightService {
         trademarkService.saveTrademark(ownTrademark);
         log.info("상표권 정보 저장 완료");
 
-        Member findMember = memberService.findById(request.getMemberId());
-        if (!memberService.existedMemberByEmail(findMember.getEmail()))
+        if(!memberService.existedMemberById(request.getMemberId()))
             throw new IllegalArgumentException("존재하지 않는 회원입니다.");
+        Member findMember = memberService.findById(request.getMemberId());
         log.info("회원 찾기 완료");
 
         MemberOwnTrademark savedInfo = memberOwnTrademarkRepository.save(new MemberOwnTrademark(ownTrademark, findMember));
