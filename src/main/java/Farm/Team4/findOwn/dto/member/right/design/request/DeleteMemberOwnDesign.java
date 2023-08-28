d677ec273be742241a67056201d0c29ddabe926a
dlawjddn <jwl5015@naver.com>
2023-08-28 21:56:51 +0900

    Feat: 회원 소유 디자인권 삭제 dto 추가

---
+++ src/main/java/Farm/Team4/findOwn/dto/member/right/design/request/DeleteMemberOwnDesign.java
@@ -0,0 +1,9 @@
+package Farm.Team4.findOwn.dto.member.right.design.request;
+
+import lombok.Data;
+
+@Data
+public class DeleteMemberOwnDesign {
+    private String memberId;
+    private Long memberOwnDesignId;
+}
