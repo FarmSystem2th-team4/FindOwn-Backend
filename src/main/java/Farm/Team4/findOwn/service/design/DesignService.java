ad53e29b31630eeaf21b4c25039cc862804c7989
dlawjddn <jwl5015@naver.com>
2023-08-29 14:10:24 +0900

    Feat: 공공 api 승인으로 origin key 사용으로 인한 수정

--- src/main/java/Farm/Team4/findOwn/service/design/DesignService.java
+++ src/main/java/Farm/Team4/findOwn/service/design/DesignService.java
@@ -16,10 +16,8 @@ import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.DefaultUriBuilderFactory;
 
 import java.io.IOException;
-import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,31 +28,19 @@ import java.util.stream.Collectors;
 public class DesignService {
     private final DesignRepository designRepository;
     private RestTemplate restTemplate = new RestTemplate();
-    @Value("${DESIGN_ENCODED_SERVICE_KEY}")
+    @Value("${DESIGN_SERVICE_KEY}")
     private String dataServiceKey;
     @Value("${SEARCH_DESIGN_URL}")
     private String searchDesignUrl;
     public List<Item> findDesign(String articleName) throws IOException {
-        /*
-            제공받은 service key가 url encoding 문제가 있음 -> '/', '+' 같은 특수 기호 때문에 똑바로 인코딩이 안 됨
-            그래서 DefaultUriBuilderFactory를 사용하고 DefaultUriBuilderFactory의 setEncodingMode를 사용해서
-            restTemplete에서 url 인코딩을 하지 않고 요청을 보내도록 했다.
-
-            -> 상표권과 다른 점은 이거지만 이거 빼면 방식은 똑같다.
-         */
-
-        DefaultUriBuilderFactory defaultUriBuilderFactory = new DefaultUriBuilderFactory();
-        defaultUriBuilderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE); //  URL encoding 사용
-
         restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8)); // Xml response
-        restTemplate.setUriTemplateHandler(defaultUriBuilderFactory);
 
         HttpHeaders headers = new HttpHeaders();
         HttpEntity<Void> request = new HttpEntity<>(headers);
         log.info("Request Message 생성완료");
 
         Response response = restTemplate.exchange(
-                searchDesignUrl + "?serviceKey=" + dataServiceKey + "&articleName=" + URLEncoder.encode(articleName, StandardCharsets.UTF_8),
+                searchDesignUrl + "?serviceKey=" + dataServiceKey + "&articleName=" + articleName,
                 HttpMethod.GET,
                 request,
                 Response.class
