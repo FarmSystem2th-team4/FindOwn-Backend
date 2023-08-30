5e712b502179329dd717c34494a3e1721473d257
dlawjddn <jwl5015@naver.com>
2023-08-30 11:32:57 +0900

    Feat: exception 추가

--- src/main/java/Farm/Team4/findOwn/exception/CustomErrorCode.java
+++ src/main/java/Farm/Team4/findOwn/exception/CustomErrorCode.java
@@ -10,7 +10,8 @@ public enum CustomErrorCode {
     NOT_FOUND_DESIGN_OF_MEMBER(400, "존재하지 않는 회원 소유 디자인입니다."),
     NOT_FOUND_TRADEMARK_OF_MEMBER(400, "존재하지 않는 회원 소유 상표권입니다"),
     NOT_MATCH_PASSWORD(400, "기존 비밀번호와 일치하지 않습니다."),
-    NOT_MATCH_MEMBER(400, "작성자와 수정자가 일치하지 않습니다.");
+    NOT_MATCH_MEMBER(400, "작성자와 수정자가 일치하지 않습니다."),
+    NOT_FOUND_REGISTERED(204, "등록 상태인 권리가 없습니다.");
     private int status;
     private String errorMessage;
     CustomErrorCode(int status, String errorMessage) {
