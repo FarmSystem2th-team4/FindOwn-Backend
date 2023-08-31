45b06f42fd90bbf148bcd6885098375c25b3e3fe
dlawjddn <jwl5015@naver.com>
2023-09-01 02:49:03 +0900

    Style: dto judgment design 구조 변경

--- src/main/java/Farm/Team4/findOwn/dto/judgment/design/request/SaveDesignJudgmentResultRequest.java
+++ src/main/java/Farm/Team4/findOwn/dto/judgment/design/request/SaveDesignJudgmentResultRequest.java
@@ -1,9 +1,9 @@
-package Farm.Team4.findOwn.dto.judgment.design;
+package Farm.Team4.findOwn.dto.judgment.design.request;
 
 import lombok.Data;
 
 @Data
-public class DesignJudgmentResult {
+public class SaveDesignJudgmentResultRequest {
     private Long applicationNumber; // 출원번호
     private String applicant; // 출원인
     private int similarity; // 유사도
