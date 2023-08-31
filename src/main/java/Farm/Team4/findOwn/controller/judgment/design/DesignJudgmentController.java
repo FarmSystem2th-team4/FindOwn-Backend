20934aa1de7e7aa965a37ce09c69a6bfff756620
dlawjddn <jwl5015@naver.com>
2023-09-01 03:25:55 +0900

    Feat: 회원 소유 디자인 판단 결과 목록 조회 로직 추가

--- src/main/java/Farm/Team4/findOwn/controller/judgment/design/DesignJudgmentController.java
+++ src/main/java/Farm/Team4/findOwn/controller/judgment/design/DesignJudgmentController.java
@@ -1,22 +1,33 @@
 package Farm.Team4.findOwn.controller.judgment.design;
 
+import Farm.Team4.findOwn.domain.judgment.DesignJudgment;
+import Farm.Team4.findOwn.dto.design.ConvertDesign;
 import Farm.Team4.findOwn.dto.judgment.design.request.SaveDesignJudgmentResultRequest;
+import Farm.Team4.findOwn.dto.judgment.design.response.GetDesignJudgmentResponse;
 import Farm.Team4.findOwn.dto.judgment.design.response.SaveDesignJudgmentResultResponse;
 import Farm.Team4.findOwn.service.judgment.DesignJudgmentService;
+import Farm.Team4.findOwn.service.member.information.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 @Slf4j
 public class DesignJudgmentController {
+    private final MemberService memberService;
     private final DesignJudgmentService designJudgmentService;
     @PostMapping("/member/design-judgment")
     public SaveDesignJudgmentResultResponse showDesignJudgmentResult(@RequestBody SaveDesignJudgmentResultRequest result) throws IOException {
         return designJudgmentService.showDesignJudgmentResult(result);
+    }
+    @GetMapping("/member/design-judgment/{memberId}")
+    public List<GetDesignJudgmentResponse> findMemberOwnDesignJudgment(@PathVariable String memberId){
+       return designJudgmentService.findMemberOwnDesignJudgment(memberId);
     }
 }
