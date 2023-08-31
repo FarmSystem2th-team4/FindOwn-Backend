97937bd249ff8100e10981e58efc9090b55aeb42
dlawjddn <jwl5015@naver.com>
2023-09-01 02:50:57 +0900

    Style: dto 이름 변경 -> controller, service 적용

--- src/main/java/Farm/Team4/findOwn/controller/judgment/design/DesignJudgmentController.java
+++ src/main/java/Farm/Team4/findOwn/controller/judgment/design/DesignJudgmentController.java
@@ -1,17 +1,11 @@
 package Farm.Team4.findOwn.controller.judgment.design;
 
-import Farm.Team4.findOwn.dto.judgment.design.DesignJudgmentResult;
-import Farm.Team4.findOwn.dto.judgment.design.ShowDesignJudgmentResult;
-import Farm.Team4.findOwn.dto.judgment.trademark.ShowTrademarkJudgmentResult;
-import Farm.Team4.findOwn.dto.judgment.trademark.TrademarkJudgmentResult;
+import Farm.Team4.findOwn.dto.judgment.design.request.SaveDesignJudgmentResultRequest;
+import Farm.Team4.findOwn.dto.judgment.design.response.SaveDesignJudgmentResultResponse;
 import Farm.Team4.findOwn.service.judgment.DesignJudgmentService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -21,8 +15,8 @@ import java.io.IOException;
 @Slf4j
 public class DesignJudgmentController {
     private final DesignJudgmentService designJudgmentService;
-    @PostMapping("/show/judgment/design")
-    public ShowDesignJudgmentResult showDesignJudgmentResult(@RequestBody DesignJudgmentResult result) throws IOException {
+    @PostMapping("/member/design-judgment")
+    public SaveDesignJudgmentResultResponse showDesignJudgmentResult(@RequestBody SaveDesignJudgmentResultRequest result) throws IOException {
         return designJudgmentService.showDesignJudgmentResult(result);
     }
 }
