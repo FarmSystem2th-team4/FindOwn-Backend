ed52ed67866529c6d46b9768262ada0e4d73ec9c
dlawjddn <jwl5015@naver.com>
2023-09-02 18:32:15 +0900

    Style: dto 구조, 이름 변경

--- src/main/java/Farm/Team4/findOwn/dto/judgment/trademark/request/SaveTrademarkJudgmentRequest.java
+++ src/main/java/Farm/Team4/findOwn/dto/judgment/trademark/request/SaveTrademarkJudgmentRequest.java
@@ -1,9 +1,9 @@
-package Farm.Team4.findOwn.dto.judgment.trademark;
+package Farm.Team4.findOwn.dto.judgment.trademark.request;
 
 import lombok.Data;
 
 @Data
-public class TrademarkJudgmentResult {
+public class SaveTrademarkJudgmentRequest {
     private Long applicationNumber; // 출원번호
     private String applicant; // 출원인
     private int similarity; // 유사도
