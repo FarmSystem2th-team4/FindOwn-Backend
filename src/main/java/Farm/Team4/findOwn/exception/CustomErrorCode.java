7443f69d8d4c44adaebd834ef4237554ce888adb
dlawjddn <jwl5015@naver.com>
2023-09-04 00:24:29 +0900

    Feat: 이메일 인증 코드 불일치 예외 추가

--- src/main/java/Farm/Team4/findOwn/exception/CustomErrorCode.java
+++ src/main/java/Farm/Team4/findOwn/exception/CustomErrorCode.java
@@ -12,6 +12,8 @@ public enum CustomErrorCode {
     NOT_FOUND_DESIGN_JUDGMENT(400, "존재하지 않은 디자인 침해 판단 결과입니다."),
     NOT_FOUND_TRADEMARK_JUDGMENT(400, "존재하지 않은 상표권 침해 판단 결과입니다."),
     NOT_MATCH_PASSWORD(400, "기존 비밀번호와 일치하지 않습니다."),
+    NOT_MATCH_CODE(400, "이메일 인증 코드가 일치하지 않습니다."),
+    NOT_MATCH_INFORMATION(400, "아이디 또는 비밀번호가 일치하지 않습니다."),
     NOT_MATCH_MEMBER(400, "작성자와 수정자가 일치하지 않습니다."),
     NOT_FOUND_REGISTERED(204, "등록 상태인 권리가 없습니다.");
     private final int status;
