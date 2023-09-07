3c93fb92d807d61965b45440b481368c4fd34ea5
dlawjddn <jwl5015@naver.com>
2023-09-08 02:27:02 +0900

    Style: dto 이름 간략화

--- src/main/java/Farm/Team4/findOwn/dto/member/information/VerifyMemberRequest.java
+++ src/main/java/Farm/Team4/findOwn/dto/member/information/VerifyMemberRequest.java
@@ -3,7 +3,7 @@ package Farm.Team4.findOwn.dto.member.information;
 import lombok.Data;
 
 @Data
-public class VerifyMemberRequestInfo {
+public class VerifyMemberRequest {
     private String email;
     private String code;
 }
