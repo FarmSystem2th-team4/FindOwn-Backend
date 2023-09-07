2551f5b6a922aa42b0eef67c503e2c37f93fe570
dlawjddn <jwl5015@naver.com>
2023-09-08 02:19:36 +0900

    Fix: member dto 이름 변경, 데이터 필드 추가

--- src/main/java/Farm/Team4/findOwn/dto/member/MemberDTO.java
+++ src/main/java/Farm/Team4/findOwn/dto/member/MemberDTO.java
@@ -5,13 +5,15 @@ import lombok.Data;
 import java.util.Date;
 
 @Data
-public class ConvertMember {
+public class MemberDTO {
     private String memberId;
+    private String email;
     private String name;
     private Date membershipDate;
 
-    public ConvertMember(String memberId, String name, Date membershipDate) {
+    public MemberDTO(String memberId, String email, String name, Date membershipDate) {
         this.memberId = memberId;
+        this.email = email;
         this.name = name;
         this.membershipDate = membershipDate;
     }
