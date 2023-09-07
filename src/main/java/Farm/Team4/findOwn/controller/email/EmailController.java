475ee0053eac2f2d770fb043e850332d5382e199
dlawjddn <jwl5015@naver.com>
2023-09-08 02:52:20 +0900

    Fix: controller 로직 단순화

--- src/main/java/Farm/Team4/findOwn/controller/email/EmailController.java
+++ src/main/java/Farm/Team4/findOwn/controller/email/EmailController.java
@@ -2,7 +2,6 @@ package Farm.Team4.findOwn.controller.email;
 
 import Farm.Team4.findOwn.dto.member.information.VerifyMemberRequest;
 import Farm.Team4.findOwn.service.email.EmailService;
-import Farm.Team4.findOwn.service.redis.RedisService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class EmailController {
     private final EmailService emailService;
-    private final RedisService redisService;
     @GetMapping("/mail")
     public String mailConfirm(@RequestParam String email) throws Exception{
         log.info("email: " + email);
@@ -21,16 +19,7 @@ public class EmailController {
     }
     @PostMapping("/mail")
     public String verifyCode(@RequestBody VerifyMemberRequest request) {
-        String redisEmailAddress = redisService.getCode(request.getCode());
-        log.info("email address from redis: " + redisEmailAddress);
-        if (redisEmailAddress.equals(request.getEmail())){
-            redisService.deleteCode(request.getCode());
-            log.info("요청 이메일 일치 = 인증 성공");
-            return "email verification success";
-        }
-        log.info("요청 이메일 불일치 = 인증 실패");
-        redisService.deleteCode(request.getCode());
-        return "email verification fail";
+        return emailService.verifyCode(request);
     }
 
 }
