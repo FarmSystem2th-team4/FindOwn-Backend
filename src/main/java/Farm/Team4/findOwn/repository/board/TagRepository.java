0c653af35124d297f80c7a360b33bb5d37a02413
dlawjddn <jwl5015@naver.com>
2023-09-27 01:46:23 +0900

    Feat: 태그 repository, 조회 로직 추가

--- src/main/java/Farm/Team4/findOwn/repository/board/TagRepository.java
+++ src/main/java/Farm/Team4/findOwn/repository/board/TagRepository.java
@@ -3,5 +3,9 @@ package Farm.Team4.findOwn.repository.board;
 import Farm.Team4.findOwn.domain.board.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    boolean existsByName(String tagName);
+    Optional<Tag> findByName(String tagName);
 }
