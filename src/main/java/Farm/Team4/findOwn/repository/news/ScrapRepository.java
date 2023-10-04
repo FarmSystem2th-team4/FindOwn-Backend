944c77ffc33c47c209776faa1b82d57db38bc296
dlawjddn <jwl5015@naver.com>
2023-10-04 17:11:23 +0900

    Feat: scrap repository 생성

---
+++ src/main/java/Farm/Team4/findOwn/repository/news/ScrapRepository.java
@@ -0,0 +1,7 @@
+package Farm.Team4.findOwn.repository.news;
+
+import Farm.Team4.findOwn.domain.news.Scrap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScrapRepository extends JpaRepository<Scrap, Long> {
+}
